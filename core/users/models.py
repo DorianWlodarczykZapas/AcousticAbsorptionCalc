8ccf8fdd93d08d482a46ed994e59efb4e47ab471
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-20 21:38:09 +0100

    feat: Implementation of rooms material model

--- core/users/models.py
+++ core/users/models.py
@@ -23,3 +23,10 @@ class Room(models.Model):
     length = models.DecimalField(max_digits=10, decimal_places=2)
     height = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class RoomMaterial(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    location = models.CharField(max_length=255)
+
