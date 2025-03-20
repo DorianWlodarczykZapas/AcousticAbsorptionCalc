375e7205a3bf271404152201d105be99e0220a99
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-20 14:03:09 +0100

    feat: Implementation of materials model

--- core/calculations/models.py
+++ core/calculations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Material(models.Model):
+    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    _120 = models.DecimalField(max_digits=22, decimal_places=2)
+    _250 = models.DecimalField(max_digits=22, decimal_places=2)
+    _500 = models.DecimalField(max_digits=22, decimal_places=2)
+    _1000 = models.DecimalField(max_digits=22, decimal_places=2)
+    _2000 = models.DecimalField(max_digits=22, decimal_places=2)
+    _4000 = models.DecimalField(max_digits=22, decimal_places=2)
+
