4266362fe4846c3b9929cca6861494edf4aff788
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-20 14:05:35 +0100

    feat: Implementation of projects model

--- core/users/models.py
+++ core/users/models.py
@@ -7,3 +7,10 @@ class User(models.Model):
     password_hash = models.CharField(max_length=255)
     role = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Project(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
