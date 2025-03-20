4931cc0c2d0281718e2c9cbc3f5d447758ba123a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-20 14:05:04 +0100

    feat: Implementation of users model

--- core/users/models.py
+++ core/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password_hash = models.CharField(max_length=255)
+    role = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
