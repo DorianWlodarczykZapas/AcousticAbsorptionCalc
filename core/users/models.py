0b26bbabad7868dfbfebd511f85a1794bdbf1c2d
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-20 21:37:29 +0100

    feat: Implementation of rooms model

--- core/users/models.py
+++ core/users/models.py
@@ -14,3 +14,12 @@ class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Room(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    width = models.DecimalField(max_digits=10, decimal_places=2)
+    length = models.DecimalField(max_digits=10, decimal_places=2)
+    height = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
