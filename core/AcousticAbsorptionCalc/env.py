6f5f592a42568cd40b8f24575718e922fdecdaa1
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-21 14:48:41 +0100

    feat: Implementation of database integration into project

--- core/AcousticAbsorptionCalc/env.py
+++ core/AcousticAbsorptionCalc/env.py
@@ -2,5 +2,6 @@ import environ
 
 env = environ.Env(
     DEBUG=(bool, False),
-    SECRET_KEY=(str, 'no-secret')
+    SECRET_KEY=(str, 'no-secret'),
+    DATABASE_URL=(str, 'postgres://username:password@localhost:port/database_name')
 )
