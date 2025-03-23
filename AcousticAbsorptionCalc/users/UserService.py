89028dd661b8ba66c57ac0605fe383c5adf78ba0
DWlodarczyk <124451052+DorianWlodarczykZapas@users.noreply.github.com>
2025-03-23 19:58:10 +0100

    feat: Implementation of get user by id and email functionality

--- AcousticAbsorptionCalc/users/UserService.py
+++ AcousticAbsorptionCalc/users/UserService.py
@@ -36,3 +36,10 @@ class UserService:
         except User.DoesNotExist:
             return None
 
+    def get_user_by_id(self, user_id: int) -> Optional[User]:
+        return User.objects.filter(id=user_id).first()
+
+    def get_user_by_email(self, email: str) -> Optional[User]:
+        return User.objects.filter(email=email).first()
+
+
