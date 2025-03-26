47513c96249e76cd50c04d60431cd6886b461cd1
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-26 21:49:07 +0100

    feat: Basic registration view implementation

--- AcousticAbsorptionCalc/users/views.py
+++ AcousticAbsorptionCalc/users/views.py
@@ -1,1 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
 
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        return render(request, "users/register.html", {"form": form})
