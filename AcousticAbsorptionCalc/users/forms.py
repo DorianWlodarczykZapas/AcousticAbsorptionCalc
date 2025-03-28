338aeb51b6e474a3e95ca907a79b9edfff6aebb4
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-28 13:05:17 +0100

    feat: Changed userregistrationclass by using forms

--- AcousticAbsorptionCalc/users/forms.py
+++ AcousticAbsorptionCalc/users/forms.py
@@ -1,19 +1,31 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.hashers import make_password
 
 from .models import User
 
 
-class UserRegistrationForm(UserCreationForm):
+class UserRegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Hasło", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Powtórz hasło", widget=forms.PasswordInput)
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Hasła muszą być takie same")
+
+        return cleaned_data
+
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data["email"]
+        user.password_hash = make_password(self.cleaned_data["password1"])
         user.role = "free_version"
         if commit:
             user.save()
