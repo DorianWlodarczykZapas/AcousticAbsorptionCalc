81eb281d29a71eaa4988b3ee1a7f98aff5b7784d
DWlodarczyk <124451052+DorianWlodarczykZapas@users.noreply.github.com>
2025-03-29 18:37:16 +0100

    feat: Changed classic register view into cbv

--- AcousticAbsorptionCalc/users/views.py
+++ AcousticAbsorptionCalc/users/views.py
@@ -1,21 +1,21 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 from projects_history.Logger import Logger
 
 from .forms import UserRegistrationForm
 
 
-def register(request):  # Class Based View - CBV
-    if request.method == "POST":
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+class RegisterView(FormView):
+    template_name = "users/register.html"
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy("users/home.html")  
 
-            Logger.log_account_creation(user_id=user.id, changed_by=user)
+    def form_valid(self, form):
+        user = form.save()
 
-            messages.success(request, f"Konto utworzone dla {user.username}")
-            return redirect("users/home.html")
-    else:
-        form = UserRegistrationForm()
+        Logger.log_account_creation(user_id=user.id, changed_by=user)
 
-    return render(request, "users/register.html", {"form": form})
+        messages.success(self.request, f"Konto utworzone dla {user.username}")
+        return super().form_valid(form)
+
