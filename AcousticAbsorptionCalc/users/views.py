3c4f2a57b63f687bd51cb7f9e91c2d16016e7a00
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-03-30 22:22:16 +0200

    Implementation of get and post login

--- AcousticAbsorptionCalc/users/views.py
+++ AcousticAbsorptionCalc/users/views.py
@@ -1,5 +1,8 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic.edit import FormView
 from projects_history.Logger import Logger
 
@@ -9,7 +12,7 @@ from .forms import UserRegistrationForm
 class RegisterView(FormView):
     template_name = "users/register.html"
     form_class = UserRegistrationForm
-    success_url = reverse_lazy("users/home.html")  
+    success_url = reverse_lazy("users/home.html")
 
     def form_valid(self, form):
         user = form.save()
@@ -19,3 +22,18 @@ class RegisterView(FormView):
         messages.success(self.request, f"Konto utworzone dla {user.username}")
         return super().form_valid(form)
 
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html")
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "login.html", {"error": "Invalid credentials"})
