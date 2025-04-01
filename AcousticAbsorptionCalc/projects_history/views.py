ab258d33bc8d1c039c0e100f0d5ac8595f5f4982
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-01 21:34:53 +0200

    feat: Implementation of class based view logs

--- AcousticAbsorptionCalc/projects_history/views.py
+++ AcousticAbsorptionCalc/projects_history/views.py
@@ -1,1 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
+from .models import ChangeLog
+
+
+class UserChangeLogListView(LoginRequiredMixin, ListView):
+    model = ChangeLog
+    template_name = "changelog/user_logs.html"
+    context_object_name = "logs"
+    paginate_by = 25
+
+    def get_queryset(self):
+        return ChangeLog.objects.filter(changed_by=self.request.user).order_by(
+            "-timestamp"
+        )
