5fd14890e2827692b193c187edddb660c1f87cc4
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-02 19:25:12 +0200

    fix: Fixed user logs class view

--- AcousticAbsorptionCalc/projects_history/views.py
+++ AcousticAbsorptionCalc/projects_history/views.py
@@ -1,16 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
-from .models import ChangeLog
+from .services import ChangeLogService
 
 
 class UserChangeLogListView(LoginRequiredMixin, ListView):
-    model = ChangeLog
     template_name = "changelog/user_logs.html"
     context_object_name = "logs"
     paginate_by = 25
 
     def get_queryset(self):
-        return ChangeLog.objects.filter(changed_by=self.request.user).order_by(
-            "-timestamp"
-        )
+        return ChangeLogService.get_user_logs(self.request.user)
