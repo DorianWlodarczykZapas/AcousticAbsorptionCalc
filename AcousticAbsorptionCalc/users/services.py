fa5bb54738e15068e8d57d4e364b215983f3cd5a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-03 13:32:53 +0200

    fix: Fixed update user method

--- AcousticAbsorptionCalc/users/services.py
+++ AcousticAbsorptionCalc/users/services.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password, make_password
 
+from .models import User
 from .repositories import UserRepository
 
 
@@ -17,3 +18,14 @@ class UserService:
     def register_user(form):
         user = form.save()
         return user
+
+    @staticmethod
+    def update_user(self, user: User, data: dict):
+        user.username = data.get("username", user.username)
+        user.email = data.get("email", user.email)
+
+        new_password = data.get("new_password")
+        if new_password:
+            user.password_hash = make_password(new_password)
+
+        user.save()
