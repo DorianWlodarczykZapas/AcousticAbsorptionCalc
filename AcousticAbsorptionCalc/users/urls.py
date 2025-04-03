b1dff6dd30ee7904e68901bf0502a55da311ac91
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-03 13:37:10 +0200

    feat: Added views to urls

--- AcousticAbsorptionCalc/users/urls.py
+++ AcousticAbsorptionCalc/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, UserProfileUpdateView
 
 urlpatterns = [
-    path("register/", RegisterView.as_view()),
-    path("login/", LoginView.as_view()),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("profile/edit/", UserProfileUpdateView.as_view(), name="edit_profile"),
 ]
