aa9e787b234921231574eb70b75cd359337a5d2f
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-03 13:27:19 +0200

    fix: Changed userregistration form class

--- AcousticAbsorptionCalc/users/forms.py
+++ AcousticAbsorptionCalc/users/forms.py
@@ -5,9 +5,17 @@ from .models import User
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(label="Hasło", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Powtórz hasło", widget=forms.PasswordInput)
-    email = forms.EmailField(required=True)
+
+    password1 = forms.CharField(
+        label="Hasło",
+        widget=forms.PasswordInput,
+        min_length=8,
+        help_text="Hasło musi mieć co najmniej 8 znaków.",
+    )
+    password2 = forms.CharField(
+        label="Powtórz hasło", widget=forms.PasswordInput, min_length=8
+    )
+    email = forms.EmailField(required=True, help_text="Wprowadź poprawny adres e-mail.")
 
     class Meta:
         model = User
@@ -18,8 +26,12 @@ class UserRegistrationForm(forms.ModelForm):
         password1 = cleaned_data.get("password1")
         password2 = cleaned_data.get("password2")
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Hasła muszą być takie same")
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError("Hasła muszą być takie same.")
+
+            if len(password1) < 8:
+                raise forms.ValidationError("Hasło musi mieć co najmniej 8 znaków.")
 
         return cleaned_data
 
@@ -27,6 +39,7 @@ class UserRegistrationForm(forms.ModelForm):
         user = super().save(commit=False)
         user.password_hash = make_password(self.cleaned_data["password1"])
         user.role = "free_version"
+
         if commit:
             user.save()
         return user
