ea7c67bb7ac1b357a15ba4fd80ada902d17e511e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-07 15:01:07 +0200

    fix: Changed user model into abstractuser version

--- AcousticAbsorptionCalc/users/models.py
+++ AcousticAbsorptionCalc/users/models.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(models.Model):  # AbstractUser
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
-    password_hash = models.CharField(max_length=255)
+class User(AbstractUser):
     role = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
