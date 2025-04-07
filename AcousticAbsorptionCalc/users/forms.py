e3462d2bd67c9be8c72dd21c68afa4116f0586eb
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-07 15:04:21 +0200

    fix: Changed user registration form depended on abstract user

--- AcousticAbsorptionCalc/users/forms.py
+++ AcousticAbsorptionCalc/users/forms.py
@@ -1,11 +1,9 @@
 from django import forms
-from django.contrib.auth.hashers import make_password
 
 from .models import User
 
 
 class UserRegistrationForm(forms.ModelForm):
-
     password1 = forms.CharField(
         label="Hasło",
         widget=forms.PasswordInput,
@@ -13,7 +11,9 @@ class UserRegistrationForm(forms.ModelForm):
         help_text="Hasło musi mieć co najmniej 8 znaków.",
     )
     password2 = forms.CharField(
-        label="Powtórz hasło", widget=forms.PasswordInput, min_length=8
+        label="Powtórz hasło",
+        widget=forms.PasswordInput,
+        min_length=8,
     )
     email = forms.EmailField(required=True, help_text="Wprowadź poprawny adres e-mail.")
 
@@ -26,18 +26,14 @@ class UserRegistrationForm(forms.ModelForm):
         password1 = cleaned_data.get("password1")
         password2 = cleaned_data.get("password2")
 
-        if password1 and password2:
-            if password1 != password2:
-                raise forms.ValidationError("Hasła muszą być takie same.")
-
-            if len(password1) < 8:
-                raise forms.ValidationError("Hasło musi mieć co najmniej 8 znaków.")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Hasła muszą być takie same.")
 
         return cleaned_data
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.password_hash = make_password(self.cleaned_data["password1"])
+        user.set_password(self.cleaned_data["password1"])
         user.role = "free_version"
 
         if commit:
