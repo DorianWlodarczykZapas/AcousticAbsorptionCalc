e6db4d9a541572c9f80aa6990030bccf0f070703
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-10 18:47:15 +0200

    feat: Creation of room form class

---
+++ AcousticAbsorptionCalc/rooms/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Room
+
+
+class RoomForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = ["name", "width", "length", "height"]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control"}),
+            "width": forms.NumberInput(attrs={"class": "form-control"}),
+            "length": forms.NumberInput(attrs={"class": "form-control"}),
+            "height": forms.NumberInput(attrs={"class": "form-control"}),
+        }
