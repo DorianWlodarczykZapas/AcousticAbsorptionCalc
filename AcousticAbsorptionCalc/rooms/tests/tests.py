f2ccf6a91be1b65d8412cf496d4d60d4809b61f5
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-10 19:00:40 +0200

    feat: Creation of test to filter rooms in project

--- AcousticAbsorptionCalc/rooms/tests/tests.py
+++ AcousticAbsorptionCalc/rooms/tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from projects.factories import ProjectFactory
 from rooms.factories import RoomFactory
 
@@ -11,3 +12,12 @@ class RoomViewsTestCase(TestCase):
         self.room1 = RoomFactory(project=self.project)
         self.room2 = RoomFactory(project=self.project)
         RoomFactory(project=self.other_project)
+
+    def test_room_list_view_filters_by_project(self):
+        url = reverse("rooms:room_list", kwargs={"project_id": self.project.id})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.room1.name)
+        self.assertContains(response, self.room2.name)
+        self.assertEqual(len(response.context["rooms"]), 2)
