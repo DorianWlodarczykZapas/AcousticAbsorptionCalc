40b76395e8ba5ebc52a6dc1b7c3fa71c3d28eab2
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-11 14:33:53 +0200

    feat: Implementation of Material factory

--- AcousticAbsorptionCalc/calculations/factories.py
+++ AcousticAbsorptionCalc/calculations/factories.py
@@ -1,6 +1,6 @@
 import factory
 
-from .models import Norm
+from .models import Material, Norm
 
 
 class NormFactory(factory.django.DjangoModelFactory):
@@ -8,3 +8,17 @@ class NormFactory(factory.django.DjangoModelFactory):
         model = Norm
 
     name = factory.Faker("word")
+
+
+class MaterialFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Material
+
+    type = factory.Faker("word")
+    name = factory.Faker("word")
+    freq_120 = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+    _250 = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+    _500 = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+    _1000 = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+    _2000 = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+    _4000 = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
