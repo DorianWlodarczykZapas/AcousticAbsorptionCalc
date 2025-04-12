5eadf19dc657bd2a3f58d23a2c14cce738b2d71a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-13 00:15:59 +0200

    feat: Created edit project option

--- AcousticAbsorptionCalc/projects/permissions.py
+++ AcousticAbsorptionCalc/projects/permissions.py
@@ -7,3 +7,13 @@ def can_view_project(user, project):
         or user.is_staff
         or SharedProject.objects.filter(project=project, shared_with_user=user).exists()
     )
+
+
+def can_edit_project(user, project):
+    return (
+        project.user == user
+        or user.is_staff
+        or SharedProject.objects.filter(
+            project=project, shared_with_user=user, access_level="edit"
+        ).exists()
+    )
