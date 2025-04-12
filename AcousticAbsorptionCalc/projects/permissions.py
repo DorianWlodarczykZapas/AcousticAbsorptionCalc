e73eeed9adcaaa1ad1d8fb9c0f2de95fc062bb2f
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-13 00:15:19 +0200

    feat: Created permissions.py

---
+++ AcousticAbsorptionCalc/projects/permissions.py
@@ -0,0 +1,9 @@
+from projects.models import SharedProject
+
+
+def can_view_project(user, project):
+    return (
+        project.user == user
+        or user.is_staff
+        or SharedProject.objects.filter(project=project, shared_with_user=user).exists()
+    )
