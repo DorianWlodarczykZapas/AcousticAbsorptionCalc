6fe0bf6a0366a0c014a616d878090352b72fd24f
DWlodarczyk <124451052+DorianWlodarczykZapas@users.noreply.github.com>
2025-04-12 20:04:08 +0200

    feat: Create views.py in plans app with planlistview class

--- AcousticAbsorptionCalc/plans/views.py
+++ AcousticAbsorptionCalc/plans/views.py
@@ -1,1 +1,9 @@
+from django.views.generic import ListView
+from .models import Plan
+
+
+class PlanListView(ListView):
+    model = Plan
+    template_name = "plans/plan_list.html"
+    context_object_name = "plans"
 
