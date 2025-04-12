19b29e6817cc4892866bf7146c45a9d9c6c163b3
DWlodarczyk <124451052+DorianWlodarczykZapas@users.noreply.github.com>
2025-04-12 20:02:32 +0200

    feat: Updated registration form with plan

--- AcousticAbsorptionCalc/users/services.py
+++ AcousticAbsorptionCalc/users/services.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import logout
-
+from django.utils.timezone import now, timedelta
 from .models import User
 from .repositories import UserRepository
+from plans.models import Plan
 
 
 class AuthService:
@@ -17,6 +18,21 @@ class UserService:
     @staticmethod
     def register_user(form):
         user = form.save()
+
+        try:
+            trial_plan = Plan.objects.get(type=Plan.PlanType.TRIAL)
+        except Plan.DoesNotExist:
+            raise Exception("Plan Trial nie został znaleziony. Upewnij się, że istnieje w bazie.")
+
+        UserPlan.objects.create(
+            user=user,
+            plan=trial_plan,
+            start_date=now().date(),
+            valid_to=now().date() + timedelta(days=7),
+            is_trial=True,
+            trial_days=7,
+        )
+
         return user
 
     @staticmethod
