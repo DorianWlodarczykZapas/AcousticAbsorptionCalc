c8f89fffe5b69c981ee57136d14e3ef920c27d6c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-14 14:06:29 +0200

    feat: Added typing into plan service services

--- AcousticAbsorptionCalc/plans/services.py
+++ AcousticAbsorptionCalc/plans/services.py
@@ -1,12 +1,19 @@
+from datetime import timedelta
+
+from django.contrib.auth.models import AbstractBaseUser
 from django.utils.timezone import now
+
 from .models import Plan, UserPlan
 
 
 class PlanService:
     @staticmethod
-    def change_user_plan(user, new_plan_type):
+    def change_user_plan(
+        user: AbstractBaseUser,
+        new_plan_type: str,
+    ) -> UserPlan:
         try:
-            new_plan = Plan.objects.get(type=new_plan_type)
+            new_plan: Plan = Plan.objects.get(type=new_plan_type)
         except Plan.DoesNotExist:
             raise Exception(f"Plan '{new_plan_type}' nie istnieje.")
 
@@ -14,6 +21,10 @@ class PlanService:
         user_plan.plan = new_plan
         user_plan.is_trial = new_plan.type == Plan.PlanType.TRIAL
         user_plan.start_date = now().date()
-        user_plan.valid_to = None if new_plan.type != Plan.PlanType.TRIAL else now().date() + timedelta(days=7)
+        user_plan.valid_to = (
+            None
+            if new_plan.type != Plan.PlanType.TRIAL
+            else now().date() + timedelta(days=7)
+        )
         user_plan.save()
         return user_plan
