8981bc70461ba34ced37d7b3382101e854caaa36
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-22 21:49:53 +0200

    feat: Class test about register form

--- AcousticAbsorptionCalc/users/tests/test_forms.py
+++ AcousticAbsorptionCalc/users/tests/test_forms.py
@@ -4,34 +4,14 @@ from users.forms import UserRegistrationForm
 pytestmark = pytest.mark.django_db
 
 
-def test_valid_user_registration_form(user_data):
-    form = UserRegistrationForm(data=user_data)
-    assert form.is_valid()
-    user = form.save()
-    assert user.username == user_data["username"]
-    assert user.email == user_data["email"]
-    assert user.check_password(user_data["password1"])
-    assert user.role == "free_version"
+class TestUserRegistrationForm:
 
+    def test_form_valid_data_creates_user(self, user_data):
+        form = UserRegistrationForm(data=user_data)
+        assert form.is_valid()
 
-def test_password_mismatch_fails():
-    data = {
-        "username": "tester",
-        "email": "tester@example.com",
-        "password1": "password123",
-        "password2": "wrongpass",
-    }
-    form = UserRegistrationForm(data=data)
-    assert not form.is_valid()
-    assert "Hasła muszą być takie same." in str(form.errors)
-
-
-def test_missing_email():
-    data = {
-        "username": "tester",
-        "password1": "password123",
-        "password2": "password123",
-    }
-    form = UserRegistrationForm(data=data)
-    assert not form.is_valid()
-    assert "email" in form.errors
+        user = form.save()
+        assert user.username == user_data["username"]
+        assert user.email == user_data["email"]
+        assert user.check_password(user_data["password1"])
+        assert user.role == "free_version"
