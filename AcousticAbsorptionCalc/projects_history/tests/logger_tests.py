b0813c07ff418043230633468ae337e74fdbdb7a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-23 21:38:53 +0200

    feat: Test in logger test class about creating account log

--- AcousticAbsorptionCalc/projects_history/tests/logger_tests.py
+++ AcousticAbsorptionCalc/projects_history/tests/logger_tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from projects_history.log_change import ChangeLog
+from projects_history.Logger import Logger
 from users.models import User
 
 
@@ -6,3 +8,13 @@ class LoggerTests(TestCase):
     def setUp(self):
         self.creator = User.objects.create(username="admin")
         self.user = User.objects.create(username="user")
+
+    def test_log_account_creation_creates_changelog_entry(self):
+        Logger.log_account_creation(user_id=self.user.id, changed_by=self.creator)
+
+        log = ChangeLog.objects.last()
+        self.assertIsNotNone(log)
+        self.assertEqual(log.entity_type, "konto")
+        self.assertEqual(log.change_type, "Utworzono konto")
+        self.assertEqual(log.entity_id, self.user.id)
+        self.assertEqual(log.changed_by, self.creator)
