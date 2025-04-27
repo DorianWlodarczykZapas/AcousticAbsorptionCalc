f9c6cfe6c64da1f4e4d735030171d6e9dc26b052
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-27 13:04:52 +0200

    feat: Implementation of password reset class model

--- AcousticAbsorptionCalc/users/models.py
+++ AcousticAbsorptionCalc/users/models.py
@@ -1,5 +1,10 @@
+import uuid
+from datetime import timedelta
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -8,3 +13,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class PasswordResetToken(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_expired(self) -> bool:
+        return timezone.now() > self.created_at + timedelta(hours=1)
+
+    def __str__(self) -> str:
+        return f"PasswordResetToken for {self.user.email}"
