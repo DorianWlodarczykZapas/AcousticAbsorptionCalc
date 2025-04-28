7c03190997cf134d5119eca2cf61c372996b018c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-28 21:23:36 +0200

    feat: Added pdf class into urls.py

--- AcousticAbsorptionCalc/projects/urls.py
+++ AcousticAbsorptionCalc/projects/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import ProjectCreateView
+from .views import ProjectCreateView, ProjectPDFView
 
 urlpatterns = [
     path("create/", ProjectCreateView.as_view(), name="project_create"),
+    path(
+        "projects/<int:project_id>/pdf/", ProjectPDFView.as_view(), name="project_pdf"
+    ),
 ]
