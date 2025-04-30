25b7349e28e9b75d081d1994c5a607a22051737a
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-30 12:29:26 +0200

    feat: Added all available polish translations

--- AcousticAbsorptionCalc/user_logs/apps.py
+++ AcousticAbsorptionCalc/user_logs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProjectsHistoryConfig(AppConfig):
+class UserLogsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "projects_history"
+    name = "user_logs"
