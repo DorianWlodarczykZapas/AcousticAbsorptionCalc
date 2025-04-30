61c49872284305df391732c5593bfc82d74024d5
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-04-30 11:58:02 +0200

    feat: Changed language in logger class

--- AcousticAbsorptionCalc/projects_history/Logger.py
+++ AcousticAbsorptionCalc/projects_history/Logger.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from users.models import User
 
 from .log_change import log_change
@@ -7,7 +8,7 @@ class Logger:
     """Wrapper for user and system logging actions."""
 
     @staticmethod
-    @log_change(entity_type="konto", change_type="Utworzono konto")
+    @log_change(entity_type=_("Account"), change_type=_("Account created"))
     def log_account_creation(user_id: int, changed_by: User) -> None:
         """
         Logs the creation of a user account.
@@ -23,31 +24,31 @@ class Logger:
             raise User.DoesNotExist(f"User with id {user_id} does not exist.")
 
     @staticmethod
-    @log_change(entity_type="pomieszczenie", change_type="Dodano")
+    @log_change(entity_type=_("Room"), change_type=_("Added"))
     def log_room_created(user_id: int, changed_by: User) -> None:
         pass
 
     @staticmethod
-    @log_change(entity_type="pomieszczenie", change_type="Zaktualizowano")
+    @log_change(entity_type=_("Room"), change_type=_("Updated"))
     def log_room_updated(user_id: int, changed_by: User) -> None:
         pass
 
     @staticmethod
-    @log_change(entity_type="pomieszczenie", change_type="Usunięto")
+    @log_change(entity_type=_("Room"), change_type=_("Deleted"))
     def log_room_deleted(user_id: int, changed_by: User) -> None:
         pass
 
     @staticmethod
-    @log_change(entity_type="projekt", change_type="Utworzono")
+    @log_change(entity_type=_("Project"), change_type=_("Created"))
     def log_project_created(user_id: int, changed_by: User) -> None:
         pass
 
     @staticmethod
-    @log_change(entity_type="projekt", change_type="Zaktualizowano")
+    @log_change(entity_type=_("Project"), change_type=_("Updated"))
     def log_project_updated(user_id: int, changed_by: User) -> None:
         pass
 
     @staticmethod
-    @log_change(entity_type="projekt", change_type="Usunięto")
+    @log_change(entity_type=_("Project"), change_type=_("Deleted"))
     def log_project_deleted(user_id: int, changed_by: User) -> None:
         pass
