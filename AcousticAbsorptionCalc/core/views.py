5a2a09c08787417faf3c86a65b3551f30cb99444
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-01 10:23:51 +0200

    fix: Changed home view structure

--- AcousticAbsorptionCalc/core/views.py
+++ AcousticAbsorptionCalc/core/views.py
@@ -4,9 +4,17 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home.html"
-    extra_context = {
-        "title": _("Acoustic Absorption Calculator"),
-        "description": _("Easily calculate and manage sound absorption projects."),
-        "login_text": _("Log in"),
-        "register_text": _("Register"),
-    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "app_title": _("Acoustic Absorption Calculator"),
+                "description": _(
+                    "Easily calculate and manage sound absorption projects."
+                ),
+                "login_text": _("Log in"),
+                "register_text": _("Register"),
+            }
+        )
+        return context
