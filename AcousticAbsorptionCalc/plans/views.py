8fd0356aab9b8c0b4834a7cb25cb1eed934fa10e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-04 21:04:42 +0200

    feat: new implementation of plan list view

--- AcousticAbsorptionCalc/plans/views.py
+++ AcousticAbsorptionCalc/plans/views.py
@@ -11,12 +11,6 @@ from plans.services import PlanService
 from .models import Plan
 
 
-class PlanListView(ListView):
-    model = Plan
-    template_name = "plans/plan_list.html"
-    context_object_name = "plans"
-
-
 class PlanChangeView(View):
     def post(
         self, request: HttpRequest, *args: Any, **kwargs: Any
@@ -30,3 +24,12 @@ class PlanChangeView(View):
         except Exception as e:
             messages.error(request, str(e))
         return redirect("plans:list")
+
+
+class PlanListView(ListView):
+    model = Plan
+    template_name = "plans/plan_list.html"
+    context_object_name = "plans"
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
