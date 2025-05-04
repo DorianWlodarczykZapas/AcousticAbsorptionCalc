9e3dbfe92818d1ed2f95a918372a9d2fcd7880ca
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-04 21:14:40 +0200

    feat: Added stripewebhook into urls

--- AcousticAbsorptionCalc/plans/urls.py
+++ AcousticAbsorptionCalc/plans/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PlanListView
+from .views import PlanListView, StripeWebhookView
 
 app_name = "plans"
 
 urlpatterns = [
     path("plans_list", PlanListView.as_view(), name="list"),
+    path("payment/webhook/", StripeWebhookView.as_view(), name="stripe_webhook"),
 ]
