01017e6b1c83976352dc14a0833e95dbc599c62b
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-05 14:20:00 +0200

    feat: Implementation of plan list view test class with first test

--- AcousticAbsorptionCalc/plans/tests.py
+++ AcousticAbsorptionCalc/plans/tests.py
@@ -1,1 +1,24 @@
+from unittest import TestCase
 
+from django.test import Client
+from django.urls import reverse
+from plans.models import Plan
+
+
+class PlanListViewTest(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        Plan.objects.create(
+            name="Base",
+            price=10,
+            billing_period="monthly",
+            max_projects=1,
+            max_rooms_per_project=1,
+            description="test",
+        )
+
+    def test_plan_list_view(self) -> None:
+        response = self.client.get(reverse("plan_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("plans", response.context)
+        self.assertTemplateUsed(response, "plans/plan_list.html")
