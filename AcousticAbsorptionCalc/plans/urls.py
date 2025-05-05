06ba2dec2647ba9c2d1102db260ef0f5bd11a353
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-05 14:11:04 +0200

    feat: Added payment cancel and success into urls.py

--- AcousticAbsorptionCalc/plans/urls.py
+++ AcousticAbsorptionCalc/plans/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import PlanListView, StripeWebhookView
+from .views import (
+    PaymentCancelView,
+    PaymentSuccessView,
+    PlanListView,
+    StripeWebhookView,
+)
 
 app_name = "plans"
 
 urlpatterns = [
     path("plans_list", PlanListView.as_view(), name="list"),
     path("payment/webhook/", StripeWebhookView.as_view(), name="stripe_webhook"),
+    path("payment/success/", PaymentSuccessView.as_view(), name="payment_success"),
+    path("payment/cancel/", PaymentCancelView.as_view(), name="payment_cancel"),
 ]
