73c9661fb9b71e13ff0d5000dd31df910ebde964
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-07 16:17:50 +0200

    fix: Added link to projects view

--- AcousticAbsorptionCalc/projects/urls.py
+++ AcousticAbsorptionCalc/projects/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
 
-from .views import ProjectCreateView, ProjectPDFView
+from .views import (
+    ProjectCreateView,
+    ProjectDeleteView,
+    ProjectDetailView,
+    ProjectListView,
+    ProjectPDFView,
+    ProjectUpdateView,
+)
+
+app_name = "projects"
 
 urlpatterns = [
+    path("", ProjectListView.as_view(), name="project_list"),
     path("create/", ProjectCreateView.as_view(), name="project_create"),
+    path("<int:pk>/", ProjectDetailView.as_view(), name="project_detail"),
+    path("<int:pk>/update/", ProjectUpdateView.as_view(), name="project_update"),
+    path("<int:pk>/delete/", ProjectDeleteView.as_view(), name="project_delete"),
     path(
-        "projects/<int:project_id>/download-pdf/",
+        "<int:project_id>/download-pdf/",
         ProjectPDFView.as_view(),
         name="project_pdf",
     ),
