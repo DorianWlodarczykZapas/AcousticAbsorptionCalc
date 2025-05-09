ea0c7693304bcbbe586f295034f2ec702082320c
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-09 22:36:30 +0200

    fix: Changed form with user

--- AcousticAbsorptionCalc/projects/forms.py
+++ AcousticAbsorptionCalc/projects/forms.py
@@ -1,9 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Project
 
 
-class ProjectForm(ModelForm):
+class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ["name", "description"]
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.user = self.user
+        if commit:
+            instance.save()
+        return instance
