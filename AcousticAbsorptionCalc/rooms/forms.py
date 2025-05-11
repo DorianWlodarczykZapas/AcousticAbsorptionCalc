faee0ccb066bb7b72800a9db35b9cb678b2b60be
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-11 22:01:01 +0200

    feat: Added norm value into room create form

--- AcousticAbsorptionCalc/rooms/forms.py
+++ AcousticAbsorptionCalc/rooms/forms.py
@@ -6,10 +6,11 @@ from .models import Room
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
-        fields = ["name", "width", "length", "height"]
+        fields = ["name", "width", "length", "height", "norm"]
         widgets = {
             "name": forms.TextInput(attrs={"class": "form-control"}),
             "width": forms.NumberInput(attrs={"class": "form-control"}),
             "length": forms.NumberInput(attrs={"class": "form-control"}),
             "height": forms.NumberInput(attrs={"class": "form-control"}),
+            "norm": forms.Select(attrs={"class": "form-control"}),
         }
