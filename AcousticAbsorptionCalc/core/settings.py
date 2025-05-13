1871a844c99840de5a5b00aaca546b261818e7f3
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-13 13:08:25 +0200

    feat: Implementation of project_logs app

--- AcousticAbsorptionCalc/core/settings.py
+++ AcousticAbsorptionCalc/core/settings.py
@@ -34,6 +34,7 @@ INSTALLED_EXTENSIONS = [
     "plans",
     "projects",
     "rooms",
+    "project_logs",
 ]
 
 INSTALLED_APPS += INSTALLED_EXTENSIONS
--- AcousticAbsorptionCalc/user_logs/migrations/0002_initial.py
+++ AcousticAbsorptionCalc/user_logs/migrations/0002_initial.py
@@ -10,16 +10,18 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ("plans", "0001_initial"),
+        ("user_logs", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name="userplan",
-            name="user",
-            field=models.OneToOneField(
-                on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL
+            model_name="changelog",
+            name="changed_by",
+            field=models.ForeignKey(
+                help_text="The user who performed the action",
+                on_delete=django.db.models.deletion.CASCADE,
+                to=settings.AUTH_USER_MODEL,
             ),
         ),
     ]
