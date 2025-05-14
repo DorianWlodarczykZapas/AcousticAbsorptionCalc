bb5cf6ce91e31d42ce14e379230441583b1b10a7
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-14 21:30:08 +0200

    feat: Changed project log model

--- AcousticAbsorptionCalc/project_logs/models.py
+++ AcousticAbsorptionCalc/project_logs/models.py
@@ -5,15 +5,29 @@ from users.models import User
 
 
 class ProjectChangeLog(models.Model):
+    class ChangeType(models.IntegerChoices):
+        CREATED = 1, "Created"
+        DELETED = 2, "Deleted"
+        SHARED = 3, "Shared"
+        DOWNLOADED = 4, "Downloaded"
+        EDIT_DIMENSIONS = 5, "Edited Dimensions"
+        EDIT_FURNISHING = 6, "Edited Furnishing"
+        EDIT_STANDARD = 7, "Edited Standard"
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     changed_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    change_type = models.CharField(max_length=50)
-    change_description = models.TextField()
+    change_type = models.IntegerField(choices=ChangeType.choices)
+
+    change_description = models.TextField(blank=True)
+
+    metadata = models.JSONField(null=True, blank=True)
+
     timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.change_type} – {self.project.name} by {self.changed_by.username}"
+        return f"{self.get_change_type_display()} – {self.project.name} by {self.changed_by.username}"
 
     class Meta:
         verbose_name = "Project Change Log"
         verbose_name_plural = "Project Change Logs"
+        ordering = ["-timestamp"]
