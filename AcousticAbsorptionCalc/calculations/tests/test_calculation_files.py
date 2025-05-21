47bd73447df4a36377447f53d9232dbf23ddf705
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-05-21 21:03:52 +0200

    feat: New implementation of tests in roomacousticcalculator

---
+++ AcousticAbsorptionCalc/calculations/tests/test_calculation_files.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from calculations.factories import NormFactory
+from calculations.RoomAcousticCalculator import RoomAcousticCalculator
+
+
+class RoomAcousticCalculatorTestCase(TestCase):
+    def setUp(self):
+        self.norm = NormFactory()
+        self.height = 3.0
+        self.length = 5.0
+        self.width = 4.0
+        self.furnishing = {"wood": 10.0}
+        self.construction = {"concrete": 20.0}
+        self.sti = 0.7
+        self.calc = RoomAcousticCalculator(
+            height=self.height,
+            length=self.length,
+            width=self.width,
+            furnishing=self.furnishing,
+            construction=self.construction,
+            norm=self.norm,
+            sti=self.sti,
+        )
