71e0e91a7622802e7eadad1ac4cf14fc78afec7e
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-03 20:59:03 +0200

    fix: Added missing slash in rooms list

--- AcousticAbsorptionCalc/rooms/urls.py
+++ AcousticAbsorptionCalc/rooms/urls.py
@@ -5,6 +5,6 @@ from .views import RoomDeleteView, RoomListView
 app_name = "rooms"
 
 urlpatterns = [
-    path("", RoomListView.as_view(), name="room_list"),
+    path("/", RoomListView.as_view(), name="room_list"),
     path("<int:pk>/delete/", RoomDeleteView.as_view(), name="room_delete"),
 ]
