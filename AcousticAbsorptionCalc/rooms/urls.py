08f71d0521545be89bdc80e46f66fb2a83e3eda7
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-03 20:53:37 +0200

    fix: Changed room list url into simpler one

--- AcousticAbsorptionCalc/rooms/urls.py
+++ AcousticAbsorptionCalc/rooms/urls.py
@@ -5,6 +5,6 @@ from .views import RoomDeleteView, RoomListView
 app_name = "rooms"
 
 urlpatterns = [
-    path("rooms/", RoomListView.as_view(), name="room_list"),
-    path("rooms/<int:pk>/delete/", RoomDeleteView.as_view(), name="room_delete"),
+    path("", RoomListView.as_view(), name="room_list"),
+    path("<int:pk>/delete/", RoomDeleteView.as_view(), name="room_delete"),
 ]
