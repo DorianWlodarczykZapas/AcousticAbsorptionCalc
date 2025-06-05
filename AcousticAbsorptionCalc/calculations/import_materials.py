f721a70ee1cf51b0ad6428dea83f7dcf471cd59f
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-05 12:07:43 +0200

    fix: Change import materials script into command

--- AcousticAbsorptionCalc/calculations/import_materials.py
+++ AcousticAbsorptionCalc/calculations/import_materials.py
@@ -1,53 +1,61 @@
 import re
 from decimal import Decimal
 
-from .models import Material
+from calculations.models import Material
+from django.core.management.base import BaseCommand
 
-SQL_DUMP_PATH = "calculations/acoustic_database.sql"
 
-materials_to_create = []
+class Command(BaseCommand):
+    help = "Import materials from SQL dump file"
 
-with open(SQL_DUMP_PATH, "r", encoding="utf-8") as f:
-    content = f.read()
+    def handle(self, *args, **kwargs):
+        SQL_DUMP_PATH = "calculations/acoustic_database.sql"
+        materials_to_create = []
 
-match = re.search(r"COPY public.materials \(.*?\) FROM stdin;(.*?)\\.", content, re.S)
+        with open(SQL_DUMP_PATH, "r", encoding="utf-8") as f:
+            content = f.read()
 
-if match:
-    materials_data = match.group(1).strip().split("\n")
-
-    for line in materials_data:
-        fields = line.split("\t")
-        if len(fields) != 8:
-            continue
-
-        (
-            _,
-            material_type,
-            name,
-            freq_125,
-            freq_250,
-            freq_500,
-            freq_1000,
-            freq_2000,
-            freq_4000,
-        ) = fields
-
-        material = Material(
-            type=material_type.strip(),
-            name=name.strip(),
-            freq_125=Decimal(freq_125),
-            freq_250=Decimal(freq_250),
-            freq_500=Decimal(freq_500),
-            freq_1000=Decimal(freq_1000),
-            freq_2000=Decimal(freq_2000),
-            freq_4000=Decimal(freq_4000),
+        match = re.search(
+            r"COPY public.materials \(.*?\) FROM stdin;(.*?)\\.", content, re.S
         )
 
-        materials_to_create.append(material)
+        if match:
+            materials_data = match.group(1).strip().split("\n")
 
-Material.objects.bulk_create(materials_to_create)
+            for line in materials_data:
+                fields = line.split("\t")
+                if len(fields) != 8:
+                    continue
 
-print(
-    _("Successfully imported %(count)d materials.")
-    % {"count": len(materials_to_create)}
-)
+                (
+                    _,
+                    material_type,
+                    name,
+                    freq_125,
+                    freq_250,
+                    freq_500,
+                    freq_1000,
+                    freq_2000,
+                    freq_4000,
+                ) = fields
+
+                material = Material(
+                    type=material_type.strip(),
+                    name=name.strip(),
+                    freq_125=Decimal(freq_125),
+                    freq_250=Decimal(freq_250),
+                    freq_500=Decimal(freq_500),
+                    freq_1000=Decimal(freq_1000),
+                    freq_2000=Decimal(freq_2000),
+                    freq_4000=Decimal(freq_4000),
+                )
+
+                materials_to_create.append(material)
+
+        Material.objects.bulk_create(materials_to_create)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully imported {len(materials_to_create)} materials."
+            )
+        )
