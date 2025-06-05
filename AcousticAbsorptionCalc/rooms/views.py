8812e9e80c00dd2accafa7c2b5923f7b83ef1939
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-05 15:05:32 +0200

    fix: Changed in dynamic possibility to input more materials

--- AcousticAbsorptionCalc/rooms/views.py
+++ AcousticAbsorptionCalc/rooms/views.py
@@ -3,7 +3,6 @@ from calculations.models import Calculation
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
-from project_logs.projectlogger import ProjectLogger
 from user_logs.logger import Logger
 
 from .forms import FurnishingFormSet, RoomForm, RoomSurfaceFormSet
@@ -38,105 +37,118 @@ class RoomCreateView(LoginRequiredMixin, CreateView):
             context["surface_formset"] = RoomSurfaceFormSet(prefix="surface")
         return context
 
-    def form_valid(self, form):
-        context = self.get_context_data()
-        furnishing_formset = context["furnishing_formset"]
-        surface_formset = context["surface_formset"]
+    class RoomCreateView(LoginRequiredMixin, CreateView):
+        model = Room
+        form_class = RoomForm
+        template_name = "rooms/room_form.html"
 
-        if furnishing_formset.is_valid() and surface_formset.is_valid():
-            form.instance.project_id = self.kwargs.get("project_id")
-            response = super().form_valid(form)
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            if self.request.POST:
+                context["surface_formset"] = RoomSurfaceFormSet(
+                    self.request.POST, prefix="surface"
+                )
+                context["furnishing_formset"] = FurnishingFormSet(
+                    self.request.POST, prefix="furnishing"
+                )
+            else:
+                context["surface_formset"] = RoomSurfaceFormSet(prefix="surface")
+                context["furnishing_formset"] = FurnishingFormSet(prefix="furnishing")
+            return context
 
-            construction_data = []
-            for surface_form in surface_formset:
-                if surface_form.cleaned_data and not surface_form.cleaned_data.get(
-                    "DELETE", False
-                ):
-                    surface = surface_form.save(commit=False)
-                    surface.room = self.object
-                    surface.save()
-                    construction_data.append(
-                        {
-                            "material": surface.material,
-                            "area_m2": surface.area,
-                        }
-                    )
+        def form_valid(self, form):
+            context = self.get_context_data()
+            surface_formset = context["surface_formset"]
+            furnishing_formset = context["furnishing_formset"]
 
-            furnishing_data = []
-            for f in furnishing_formset:
-                if f.cleaned_data and not f.cleaned_data.get("DELETE", False):
-                    Furnishing.objects.create(
-                        room=self.object,
-                        name=f.cleaned_data["material"].name,
-                        material=f.cleaned_data["material"],
-                        quantity=f.cleaned_data["area"],
-                    )
-                    furnishing_data.append(
-                        {
-                            "material": f.cleaned_data["material"],
-                            "area_m2": f.cleaned_data["area"],
-                        }
-                    )
+            if surface_formset.is_valid() and furnishing_formset.is_valid():
+                form.instance.project_id = self.kwargs.get("project_id")
+                self.object = form.save()
 
-            norm = self.object.norm
-            calculator = AcousticCalculator(
-                norm=norm,
-                room_dimensions={
-                    "height": float(self.object.height),
-                    "length": float(self.object.length),
-                    "width": float(self.object.width),
-                },
-                construction_surfaces=construction_data,
-                furnishing_elements=furnishing_data,
-                freq_band="500",
-            )
+                # Save Surfaces
+                construction_data = []
+                for surface_form in surface_formset:
+                    if surface_form.cleaned_data and not surface_form.cleaned_data.get(
+                        "DELETE", False
+                    ):
+                        surface = surface_form.save(commit=False)
+                        surface.room = self.object
+                        surface.save()
+                        construction_data.append(
+                            {
+                                "material": surface.material,
+                                "area_m2": surface.area,
+                            }
+                        )
 
-            validation_result = calculator.validate_surface_match()
+                # Save Furnishings
+                furnishing_data = []
+                for furnishing_form in furnishing_formset:
+                    if (
+                        furnishing_form.cleaned_data
+                        and not furnishing_form.cleaned_data.get("DELETE", False)
+                    ):
+                        Furnishing.objects.create(
+                            room=self.object,
+                            name=furnishing_form.cleaned_data["material"].name,
+                            material=furnishing_form.cleaned_data["material"],
+                            quantity=furnishing_form.cleaned_data["area"],
+                        )
+                        furnishing_data.append(
+                            {
+                                "material": furnishing_form.cleaned_data["material"],
+                                "area_m2": furnishing_form.cleaned_data["area"],
+                            }
+                        )
 
-            if not validation_result["valid"]:
-                print(
-                    "[WARNING] Provided construction surface area does not match geometry: "
-                    f"expected={validation_result['expected_area']} m², "
-                    f"provided={validation_result['provided_area']} m², "
-                    f"difference={validation_result['difference']} m²"
+                # Acoustic Calculation
+                norm = self.object.norm
+                calculator = AcousticCalculator(
+                    norm=norm,
+                    room_dimensions={
+                        "height": float(self.object.height),
+                        "length": float(self.object.length),
+                        "width": float(self.object.width),
+                    },
+                    construction_surfaces=construction_data,
+                    furnishing_elements=furnishing_data,
+                    freq_band="500",
                 )
 
-            rt = calculator.calculate_rt()
-            sti = round(0.75 - rt * 0.2, 2)
-            is_within = calculator.is_within_norm(rt)
+                validation_result = calculator.validate_surface_match()
 
-            Calculation.objects.create(
-                norm=norm,
-                room_height=self.object.height,
-                room_volume=calculator.room_volume,
-                room_surface_area=calculator.room_surface_area,
-                reverberation_time=rt,
-                sti=sti,
-                required_absorption=calculator.calculate_required_absorption(),
-                achieved_absorption=calculator.calculate_absorption(),
-                is_within_norm=is_within,
+                if not validation_result["valid"]:
+                    print(
+                        "[WARNING] Provided construction surface area does not match geometry: "
+                        f"expected={validation_result['expected_area']} m², "
+                        f"provided={validation_result['provided_area']} m², "
+                        f"difference={validation_result['difference']} m²"
+                    )
+
+                rt = calculator.calculate_rt()
+                sti = round(0.75 - rt * 0.2, 2) if rt else 0.0
+                is_within = calculator.is_within_norm(sti)
+
+                Calculation.objects.create(
+                    norm=norm,
+                    room_height=self.object.height,
+                    room_volume=calculator.room_volume,
+                    room_surface_area=calculator.room_surface_area,
+                    reverberation_time=rt,
+                    sti=sti,
+                    required_absorption=calculator.calculate_required_absorption(),
+                    achieved_absorption=calculator.calculate_absorption(),
+                    is_within_norm=is_within,
+                )
+
+                return super().form_valid(form)
+            else:
+                return self.form_invalid(form)
+
+        def get_success_url(self):
+            return reverse_lazy(
+                "rooms:room_list", kwargs={"project_id": self.kwargs.get("project_id")}
             )
-
-            Logger.log_room_created(
-                user_id=self.object.pk, changed_by=self.request.user
-            )
-            ProjectLogger.log_room_created(
-                project=self.object.project,
-                changed_by=self.request.user,
-                change_description=("Room '{name}' created").format(
-                    name=form.cleaned_data["name"]
-                ),
-                metadata={f["material"].name: f["area_m2"] for f in furnishing_data},
-            )
-
-            return response
-        else:
-            return self.form_invalid(form)
-
-    def get_success_url(self):
-        return reverse_lazy(
-            "rooms:room_list", kwargs={"project_id": self.kwargs.get("project_id")}
-        )
 
 
 class RoomUpdateView(LoginRequiredMixin, UpdateView):
