5e2450c3a0dbeaf54c58565bcec4782a5460e54d
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-05 12:00:59 +0200

    feat: Added missing url about creating room

--- AcousticAbsorptionCalc/rooms/urls.py
+++ AcousticAbsorptionCalc/rooms/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import RoomDeleteView, RoomListView
+from .views import RoomCreateView, RoomDeleteView, RoomListView
 
 app_name = "rooms"
 
 urlpatterns = [
     path("", RoomListView.as_view(), name="room_list"),
     path("<int:pk>/delete/", RoomDeleteView.as_view(), name="room_delete"),
+    path("create/", RoomCreateView.as_view(), name="room_create"),
 ]
