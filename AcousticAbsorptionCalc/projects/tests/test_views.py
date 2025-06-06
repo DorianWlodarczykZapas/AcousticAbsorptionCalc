033850c3b470f0c3105ee3788a21c65b4e0f1847
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-06 15:42:53 +0200

    Test: Testing generating csv file case

--- AcousticAbsorptionCalc/projects/tests/test_views.py
+++ AcousticAbsorptionCalc/projects/tests/test_views.py
@@ -176,3 +176,9 @@ class ProjectCSVViewTest(TestCase):
         self.user = UserFactory()
         self.project = ProjectFactory(user=self.user)
         self.client.force_login(self.user)
+
+    def test_generate_csv_success(self):
+        url = reverse("projects:project_csv", args=[self.project.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "text/csv")
