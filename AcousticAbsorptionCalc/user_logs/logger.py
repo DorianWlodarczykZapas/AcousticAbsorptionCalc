de94936cc5400fed2422f6129600227bf7281047
DorianWlodarczykZapas <wlodarczyk10000000@gmail.com>
2025-06-11 20:57:10 +0200

    feat: the method responsible for adding the user registration log has been extended

--- AcousticAbsorptionCalc/user_logs/logger.py
+++ AcousticAbsorptionCalc/user_logs/logger.py
@@ -1,3 +1,5 @@
+from django.utils.timezone import now
+from django.utils.translation import gettext as _
 from users.models import User
 
 from .log_change import log_change
@@ -9,11 +11,34 @@ class Logger:
 
     @staticmethod
     @log_change(
-        entity_type=ChangeLog.EntityType.USER, change_type=ChangeLog.ChangeType.CREATE
+        entity_type=ChangeLog.EntityType.USER,
+        change_type=ChangeLog.ChangeType.CREATE,
     )
     def log_account_creation(user_id: int, changed_by: User) -> None:
         if not User.objects.filter(pk=user_id).exists():
             raise User.DoesNotExist(f"User with id {user_id} does not exist.")
+
+        log_entry = (
+            ChangeLog.objects.filter(
+                entity_type=ChangeLog.EntityType.USER,
+                entity_id=user_id,
+                changed_by=changed_by,
+                change_type=ChangeLog.ChangeType.CREATE,
+            )
+            .order_by("-timestamp")
+            .first()
+        )
+
+        if log_entry:
+            log_entry.metadata = {
+                "description": _("Account created for %(username)s (%(email)s)")
+                % {
+                    "username": changed_by.username,
+                    "email": changed_by.email,
+                },
+                "timestamp": now().isoformat(),
+            }
+            log_entry.save()
 
     @staticmethod
     @log_change(
